64b19e285a171c547ea5da6d60e48814412aaf87
Illia KUtnik <illiakutnik@gmail.com>
2020-01-20 20:51:09 +0200

    vs

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	res.send('Api Running')
+	res.send('Api is running')
 })
 
 const PORT = process.env.PORT || 5000
